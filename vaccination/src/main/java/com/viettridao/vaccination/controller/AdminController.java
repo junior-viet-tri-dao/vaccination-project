cc3550ec614b18351c6e42e998b1184849791e90
NguyenVanCao-98 <hoathinhhay98@gmail.com>
2025-09-26 20:53:40 +0700

    update cao

--- vaccination/src/main/java/com/viettridao/vaccination/controller/AdminController.java
+++ vaccination/src/main/java/com/viettridao/vaccination/controller/AdminController.java
@@ -8,14 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-//    // Hiển thị giao diện Dashboard khi người dùng truy cập /dashboard
-//    @GetMapping("/dashboard")
-//    public String showDashboard(Model model) {
-//        // Nếu cần, có thể thêm attribute cho dashboard ở đây:
-//        // model.addAttribute("pageTitle", "Dashboard Trang quản trị");
-//        return "adminPanel/dashboard";
-//    }
-
     // Khi người dùng truy cập /admin/schedule sẽ hiển thị trang schedule
     @GetMapping("/schedule")
     public String showSchedulePage() {
